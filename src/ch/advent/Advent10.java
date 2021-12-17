86ab356ba90d3556274f05e0fcab559bb44d8eb7
murers <63048925+murersDHBW@users.noreply.github.com>
2021-12-17 23:18:33 +0100

    Day 5

---
+++ src/ch/advent/Advent10.java
@@ -0,0 +1,84 @@
+package ch.advent;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Advent10 {
+    public Stack<String> open;
+    public Stack<String> close;
+    public static List<String> isOpen = Arrays.asList("[","{","(","<");
+
+    public static void main(String[] args) throws IOException {
+
+
+        File file = new File("C:\\Users\\silas\\Downloads\\advent10.txt");
+        Scanner scanner = new Scanner(file);
+        List<String> input = new ArrayList<>();
+        while (scanner.hasNext()) {
+            input.add(scanner.nextLine());
+            System.out.println("File read");
+        }}
+
+    private static void part1() {
+        // Idee: Schauen ob von der Art her die letzte Klammer geschlossen wird
+        //List<Character> illegalChar = input.stream().map(char)
+
+
+
+
+
+    }
+
+//    private static void checker(String openChar, String nextChar) {
+//        long sum = 0;
+//
+//            if (openChar.equals(correctChar)){
+//
+//
+//            }
+//
+//        sum += switch(nextChar) {
+//        case ')'-> 3;
+//        case '>' -> 25137;
+//        case ']' -> 57;
+//        case '}' -> 1197;
+//        default -> throw new IllegalArgumentException();
+//
+//            System.out.println(sum);
+//    };
+//
+//        }
+
+    private static Map<Character, Integer> createLegalsScoreMap() {
+        HashMap<Character, Integer> map = new HashMap<>();
+        map.put(')', 1);
+        map.put(']', 2);
+        map.put('}', 3);
+        map.put('>', 4);
+        return map;
+    }
+
+
+    private static Map<Character, Integer> createIllegalsScoreMap() {
+        HashMap<Character, Integer> map = new HashMap<>();
+        map.put(')', 3);
+        map.put(']', 57);
+        map.put('}', 1197);
+        map.put('>', 25137);
+        return map;
+    }
+
+    private static Map<Character, Character> createOpenCloseMap() {
+        HashMap<Character, Character> map = new HashMap<>();
+        map.put('(', ')');
+        map.put('[', ']');
+        map.put('{', '}');
+        map.put('<', '>');
+        return map;
+    }
+}
--- src/ch/advent/Advent5.java
+++ src/ch/advent/Advent5.java
@@ -3,21 +3,75 @@ package ch.advent;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Advent5 {
     public static void main(String[] args) throws IOException {
-        String path = "C:\\Users\\silas\\Downloads\\advent5.txt";
-        List<Integer> file = Files.lines(Paths.get(path)).flatMap(s -> Stream.of(s.split(","))).map(Integer::valueOf).collect(Collectors.toList());
+        String path = "C:\\Users\\silas\\IdeaProjects\\AoC2021\\advent5.txt";
+        HashMap<Integer, Integer> map = new HashMap<>();
+        List<String> file = Files.lines(Paths.get(path)).map(String::valueOf).collect(Collectors.toList());
+        System.out.println(file);
+        //file.stream().map(t -> Stream.of(t.split(",")).map(Integer::valueOf)).collect(Collectors.toList(map.put()));
 
 
-        part1();
+        System.out.println(part51(file).getDangerZones());
     }
 
-    private static void part1() {
-
-
+    public static Coordinate part51(List<String> list) {
+        Coordinate map = new Coordinate();
+        for (String line : list) {
+            Matcher match = Pattern.compile("(\\d*),(\\d*) -> (\\d*),(\\d*)").matcher(line);
+            match.find();
+            int[] c = new int[]{
+                    Integer.parseInt(match.group(1)),
+                    Integer.parseInt(match.group(2)),
+                    Integer.parseInt(match.group(3)),
+                    Integer.parseInt(match.group(4))};
+            if (c[0] == c[2] && c[1] != c[3]) {
+                map.add(c[0], c[1]);
+                int changeH = c[1] > c[3] ? -1 : +1;
+                while (c[1] != c[3]) {
+                    c[1] += changeH;
+                    map.add(c[0], c[1]);
+                }
+            } else if (c[0] != c[2] && c[1] == c[3]) {
+                map.add(c[0], c[1]);
+                int changeV = c[0] > c[2] ? -1 : +1;
+                while (c[0] != c[2]) {
+                    c[0] += changeV;
+                    map.add(c[0], c[1]);
+                }
+            }
+            // diagonal
+            else {
+                map.add(c[0], c[1]);
+                int[] changeD = new int[]{c[0] > c[2] ? -1 : +1, c[1] > c[3] ? -1 : +1};
+                while (c[0] != c[2]) {
+                    c[0] += changeD[0];
+                    c[1] += changeD[1];
+                    map.add(c[0], c[1]);
+                }
+            }
+        }
+        return map;
     }
+
+    static class Coordinate {
+        private int[][] map = new int[1000][1000];
+        private int dangerZones = 0;
+
+        public int getDangerZones() {
+            return dangerZones;
+        }
+
+        public void add(int x, int y) {
+            if (++map[x][y] == 2) dangerZones++;
+        }
+    }
+
+
 }
